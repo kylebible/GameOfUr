cd7803976e236d6463f12c608ff681037fe34f06
KYLE C BIBLE <kylecbible@MacBook-Pro.local>
2017-07-15 16:34:00 -0700

    More logic, moved script tag

--- game.js
+++ game.js
@@ -10,10 +10,12 @@ var GameSpace = function(coordinate) {
     this.isReRoll = false
 }
 
-var Player = function(gameSpace) {
+var Player = function(gameSpace,player) {
     this.pieces = {}
     this.score = 0
     this.path = []
+    this.finishedPieces = 0
+    this.player = player
 }
 
 var Game = function() {
@@ -28,7 +30,14 @@ var Game = function() {
         for (var i=0;i<8;i++) {
             gameBoardRow = []
             for(var j=0;j<3;j++) {
-                gameBoardRow.push(new GameSpace(i+"x"+j))
+                newSpace = new GameSpace(i+"x"+j)
+                gameBoardRow.push(newSpace)
+                if((i==0&&(j==0||j==2))||(i==6&&(j==0||j==2))) {
+                    newSpace.isReRoll = true
+                }
+                if((j==1&&(i==1||i==4))) {
+                    newSpace.isSafe = true
+                }
             }
             gameBoard.push(gameBoardRow)
         }
@@ -37,8 +46,8 @@ var Game = function() {
     }
 
     Game.prototype.createPlayers = function() {
-        this.player1 = new Player(this.gameBoard[4][0])
-        this.player2 = new Player(this.gameBoard[4][2])
+        this.player1 = new Player(this.gameBoard[4][0],1)
+        this.player2 = new Player(this.gameBoard[4][2],2)
         var x=4, separate=true
         for(var i=1;i<=16;i++) {
             if (separate) {
@@ -63,48 +72,83 @@ var Game = function() {
             }
         }
         for (var count=1;count<=5;count++) {
-            this.player1.pieces["piece"+count] = new GamePiece(0,this.player1)
-            this.player2.pieces["piece"+count] = new GamePiece(0,this.player2)
+            this.player1.pieces["blue"+count] = new GamePiece(0,this.player1)
+            this.player2.pieces["red"+count] = new GamePiece(0,this.player2)
         }
         return this
     }
 
     Game.prototype.rollDice = function() {
         console.log("roll")
-        roll = 2
+        roll = 1
         //dicelogic goes here
-         //player selects piece to move
-        selectedPiece = this.playerTurn.pieces.piece1;
-        if (selectedPiece && selectedPiece.ownedBy == this.playerTurn) {
-            if (this.playerTurn.path[selectedPiece.pathIndex+roll].currentPiece) {
-              console.log("Player on space")
+        //player selects piece to move
+        $("Player"+this.playerTurn.player).click(function() { selectedPiece = $(this).attr('id')
+            if (selectedPiece && selectedPiece.ownedBy == this.playerTurn) {
+                if (selectedPiece.pathIndex+roll>=15) { //if they roll over the end
+                    if(selectedPiece.pathIndex+roll==15) { //if they roll the right amount
+                        this.playerTurn.path[selectedPiece.pathIndex].currentPiece = null
+                        selectedPiece.pathIndex+=roll
+                        this.playerTurn.finishedPieces+=1
+                        if (this.finishedPieces>=5) {
+                            //win condition
+                            console.log("player"+this.playerTurn.player+"wins")
+                        }
+                    }
+                    else {
+                        //you need to roll exactly x to remove the game piece
+                    }
+                }
+                if (this.playerTurn.path[selectedPiece.pathIndex+roll].currentPiece.ownedBy != this.playerTurn) {
+                    if (this.playerTurn.path[selectedPiece.pathIndex+roll].isSafe) {
+                        //space is safe
+                    }
+                    else {
+                    //remove other player piece, replace with currplayer piece
+                    this.playerTurn.path[selectedPiece.pathIndex+roll].currentPiece.pathIndex = 0
+                    selectedPiece.pathIndex+=roll
+                    this.playerTurn.path[selectedPiece.pathIndex].currentPiece = selectedPiece
+                    }
+                }
+                else if (this.playerTurn.path[selectedPiece.pathIndex+roll].currentPiece.ownedBy == this.playerTurn) {
+                //can't do it, one of your pieces is there
+                }
+                else {
+                    //no players on space, move piece
+                    this.playerTurn.path[selectedPiece.pathIndex].currentPiece = null
+                    selectedPiece.pathIndex+=roll
+                    this.playerTurn.path[selectedPiece.pathIndex].currentPiece = selectedPiece
+                }
             }
             else {
-              console.log('No Player Present')
-              this.playerTurn.path[selectedPiece.pathIndex].currentPiece = null
-              selectedPiece.pathIndex+=roll
-              this.playerTurn.path[selectedPiece.pathIndex].currentPiece = selectedPiece
+                //no piece on space or not your piece
             }
-        }
-        else {
-            //no piece on space or not your piece
-        }
+            $("div").off("click")
+        })
+        
     }
 
 
 
 }
 
+
+
 var myGame = new Game();
 
 myGame.setGameBoard().createPlayers()
 
+$("button").click(function() {
+    console.log("button clicked")
+    myGame.rollDice()
+})
+
 myGame.playerTurn = myGame.player1
-console.log(myGame.gameBoard)
-console.log(myGame.player1.path)
-console.log(myGame.playerTurn.pieces)
-myGame.rollDice()
-console.log(myGame.gameBoard)
+// console.log(myGame.gameBoard)
+// console.log(myGame.player1.path)
+// console.log(myGame.playerTurn.pieces)
+// myGame.rollDice()
+// console.log(myGame.gameBoard)
 
 
 
